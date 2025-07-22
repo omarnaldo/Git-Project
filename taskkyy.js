5de99e397dfe619c0043d0979e773385a51a4558
Omar <ioomxr@icloud.com>
2025-07-22 13:38:31 +0300

    just removed a space from the HTML file and added some lines to the .gitignore file. Here is the commit message:

---
+++ taskkyy.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var fullName = firstName + " " + lastName
+alert("Hello, " + fullName + "! Welcome to Taskyy.");
